d9f08de2f177b57914f829de9d86780b28e7aaa9
Odit <khadijahpassa@gmail.com>
2025-12-03 13:12:41 +0700

    resolve font issue and refine footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-10 items-start">
 
         {/* Left: Logo + Quote */}
-        <div className="space-y-4">
+        <div className="space-y-6">
           <div className="flex items-center gap-3">
             <img 
               src="/logo.png" 
@@ -22,22 +22,22 @@ const Footer = () => {
         </div>
 
         {/* Middle: Menu */}
-        <div className="space-y-3">
+        <div className="space-y-6">
           <h3 className="text-xl font-semibold">Menu</h3>
-          <ul className="space-y-2 text-lg">
-            <li><a href="#" className="hover:text-teal-200">Beranda</a></li>
+          <ul className="space-y-2 text-md">
+            <li><a href="#" className="hover:text-teal-200">Home</a></li>
             <li><a href="#" className="hover:text-teal-200">About Us</a></li>
+            <li><a href="#" className="hover:text-teal-200">How It Works</a></li>
             <li><a href="#" className="hover:text-teal-200">Lapor Sampah</a></li>
-            <li><a href="#" className="hover:text-teal-200">Trash Whisperer</a></li>
           </ul>
         </div>
 
         {/* Right: CTA */}
-        <div className="space-y-4">
+        <div className="space-y-6">
           <h3 className="text-3xl font-semibold leading-snug">
             Buat perubahan mulai dari satu laporan.
           </h3>
-          <button className="flex items-center gap-2 bg-teal-100 text-teal-900 px-5 py-3 rounded-lg hover:bg-teal-200 transition">
+          <button className="cursor-pointer flex items-center gap-2 hover:gap-4 bg-[#CCFFF3] text-[#00664E] px-5 py-3 rounded-lg hover:bg-[#99FFE7] transition-all duration-300">
             <Send size={20}/>
             Mulai Laporkan Sekarang
           </button>
@@ -45,8 +45,8 @@ const Footer = () => {
 
       </div>
 
-      <div className="border-t border-white/50 mt-12 pt-6 text-center text-sm">
-        Copyright © {new Date().getFullYear()} Menyuarakan. All rights reserved
+      <div className="border-t border-white/50 mt-12 pt-6 text-center font-light text-sm">
+        Copyright © {new Date().getFullYear()} <span className='font-semibold'>Menyuarakan</span>. All rights reserved
       </div>
     </footer>
   );
