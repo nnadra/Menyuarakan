912ad6264d75e4e0bcd8a29a0cfe3f1c798a2f3c
nnadra <nailanadrafahira@gmail.com>
2025-12-14 23:07:22 +0700

    use HashRouter for gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-
-  <BrowserRouter basename="/Menyuarakan">
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>,
 )
