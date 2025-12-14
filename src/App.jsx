5feee3db32e580a00a3d19024da5358310e4e57c
nnadra <nailanadrafahira@gmail.com>
2025-12-14 23:19:46 +0700

    remove index

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
-import Navbar from './components/Navbar.jsx'
-import Footer from './components/Footer.jsx'
-import LandingPage from './pages/LandingPage.jsx'
-import ReportPage from './pages/ReportPage.jsx'
-import CategoriesPage from './pages/CategoriesPage.jsx'
-import DashboardPage from './pages/Dashboard.jsx'
-import SignInPage from './pages/SignIn.jsx'
-import SignUpPage from './pages/SignUp.jsx'
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
+import LandingPage from './pages/LandingPage.jsx';
+import ReportPage from './pages/ReportPage.jsx';
+import CategoriesPage from './pages/CategoriesPage.jsx';
+import DashboardPage from './pages/Dashboard.jsx';
+import SignInPage from './pages/SignIn.jsx';
+import SignUpPage from './pages/SignUp.jsx';
 
 function App() {
+
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
 
-      <div className="flex-1">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/report" element={<ReportPage />} />
-          <Route path="/categories" element={<CategoriesPage />} />
-          <Route path="/signin" element={<SignInPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-        </Routes>
+          <div className="flex-1">
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/report" element={<ReportPage />} />
+              <Route path="/categories" element={<CategoriesPage />} />
+              <Route path="/signin" element={<SignInPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
+              <Route path="/dashboard" element={<DashboardPage />} />
+            </Routes>
+          </div>
+
+        <Footer />
       </div>
-
-      <Footer />
-    </div>
+    </Router>
   )
 }
 
